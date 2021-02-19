f113964076a5cfeaa1e4c80183989da7ede5799c
hysteria47 <hysteria47@qq.com>
2021-02-19 12:48:51 +0800

    用户注册登录界面上传

---
+++ log/register_do.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hp
+ * Date: 2021/1/31
+ * Time: 14:53
+ */
+header("Content-Type: text/html;charset=utf-8");
+include_once ("../conn/conn.php");
+$user=$_GET["user1"];
+$password=$_GET['password'];
+$qq=$_GET['qq'];
+$sql1 = "select * from tb_user where username='$user'";
+$check = mysqli_query($conn, $sql1);
+$info1 = mysqli_fetch_array($check);
+if ($info1==true) {
+    ;echo "<script charset='gb2312' type='text/javascript'>alert('用户名已存在，请重新写');location='javascript:history.back()';</script>";
+}
+else {
+    $sql1 = "insert into tb_user(username,pwd,qq) values('$user','$password','$qq')";
+    $result = $conn->query($sql1);
+    if ($result) {
+        echo "添加成功,2秒后返回登入界面";
+        header("Refresh:2;url=login.php");
+    } else {
+        echo "注册失败,2秒后返回注册界面";
+        header("Refresh:2;url=register.php");
+    }
+
+}
