002649a423f101948c9251c46c6e6f2529e943b6
hysteria47 <hysteria47@qq.com>
2021-02-19 12:58:03 +0800

    其他上传

---
+++ admin/main.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>后台管理</title>
+		
+		<link rel="stylesheet" type="text/css" href="style.css" />	
+	
+	</head>
+	
+		<frameset rows="150,*" >
+			<frame src="head.html" />
+			<frameset cols="200,*">
+				<frame src="left.html" style="background-color: #000000;" />
+				<frame src="body.html" name="body" />
+			</frameset>
+		</frameset>
+	
+</html>
